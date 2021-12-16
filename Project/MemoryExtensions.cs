55eebb523e71b1364b99901959c11942cb11ec43
David Smith <kittoes0124@byteterrace.com>
2021-12-15 22:59:33 -0600

    renamed split to delimit

--- Project/MemoryExtensions.cs
+++ Project/MemoryExtensions.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Toolkit.HighPerformance.Buffers;
-using System.Buffers;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 namespace ByteTerrace.Ouroboros.Core
 {
@@ -9,99 +10,160 @@ namespace ByteTerrace.Ouroboros.Core
     /// </summary>
     public static class MemoryExtensions
     {
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-        public static IReadOnlyList<ReadOnlyMemory<char>> Split(
-            this ReadOnlyMemory<char> value,
-            char delimiter,
-            char escapeSentinel,
-            ArrayPoolBufferWriter<int> indicesBuffer,
-            ArrayPoolBufferWriter<char> stringBuffer,
-            int? expectedCount = default
-        ) {
-            indicesBuffer.Clear();
-            value.Span.IndicesOf(
-                buffer: indicesBuffer,
-                value0: delimiter,
-                value1: escapeSentinel
-            );
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ReadOnlyMemory<T> Concat<T>(this ReadOnlyMemory<T> input, ReadOnlyMemory<T> other) {
+            var result = new T[(input.Length + other.Length)].AsMemory();
 
-            var loopLimit = indicesBuffer.WrittenCount;
-            var results = new List<ReadOnlyMemory<char>>(capacity: (expectedCount ?? 16));
+            input.CopyTo(result);
+            other.CopyTo(result[input.Length..]);
 
-            if (0 < loopLimit) {
-                var isNotEscaping = true;
-                var loopIndex = 0;
-                var previousIndex = 0;
-                var temp = ReadOnlyMemory<char>.Empty;
+            return result;
+        }
+        public static ReadOnlyMemory<ReadOnlyMemory<char>> Delimit(this ReadOnlyMemory<char> input, char delimiter, char escapeSentinel) {
+            static ReadOnlySpan<int> MakeDelimiterList(ref ValueListBuilder<int> valueListBuilder, ref char input, int length, char delimiter, char escapeSentinel) {
+                var index = 0;
 
-                do {
-                    var currentIndex = indicesBuffer.WrittenSpan[loopIndex];
+                if (Sse41.IsSupported) {
+                    if (17 < length) {
+                        index = MakeDelimiterListVectorized(ref valueListBuilder, ref input, length, delimiter, escapeSentinel);
+                    }
+                }
 
-                    if (escapeSentinel == value.Span[currentIndex]) {
-                        isNotEscaping = !isNotEscaping;
+                for (; (index < length); ++index) {
+                    var c = Unsafe.Add(ref input, ((IntPtr)(uint)index));
 
-                        if (previousIndex != currentIndex) {
-                            if (0 < temp.Length) {
-                                stringBuffer.Write(temp.Span);
-                            }
+                    if ((c == delimiter) || (c == escapeSentinel)) {
+                        valueListBuilder.Append(index);
+                    }
+                }
 
-                            temp = value[previousIndex..currentIndex];
+                return valueListBuilder.AsSpan();
+            }
+            static int MakeDelimiterListVectorized(ref ValueListBuilder<int> valueListBuilder, ref char input, int length, char delimiter, char escapeSentinel) {
+                var delimiterVector = Vector128.Create(delimiter);
+                var escapeSentinelVector = Vector128.Create(escapeSentinel);
+                var index = 0;
+                var shuffleConstant = Vector128.Create(0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF);
+                var vectorLength = (length & (-Vector128<ushort>.Count));
+
+                for (; (index < vectorLength); index += Vector128<ushort>.Count) {
+                    ref var c = ref Unsafe.Add(ref input, ((IntPtr)(uint)index));
+                    ref var b = ref Unsafe.As<char, byte>(ref c);
+
+                    var charVector = Unsafe.ReadUnaligned<Vector128<ushort>>(ref b);
+                    var compareVector = Sse2.CompareEqual(charVector, delimiterVector);
+
+                    compareVector = Sse2.Or(Sse2.CompareEqual(charVector, escapeSentinelVector), compareVector);
+
+                    if (Sse41.TestZ(compareVector, compareVector)) { continue; }
+
+                    var maskVector = Sse2.ShiftRightLogical(compareVector.AsUInt64(), 4).AsByte();
+                    maskVector = Ssse3.Shuffle(maskVector, shuffleConstant);
+                    var lowBits = Sse2.ConvertToUInt32(maskVector.AsUInt32());
+                    maskVector = Sse2.ShiftRightLogical(maskVector.AsUInt64(), 32).AsByte();
+                    var highBits = Sse2.ConvertToUInt32(maskVector.AsUInt32());
+
+                    for (var i = index; (0 != lowBits); ++i) {
+                        if (0 != (lowBits & 0xF)) {
+                            valueListBuilder.Append(i);
                         }
 
-                        previousIndex = ++currentIndex;
+                        lowBits >>= 8;
                     }
-                    else if (isNotEscaping && (delimiter == value.Span[currentIndex])) {
-                        if (0 == temp.Length) {
-                            results.Add(value[previousIndex..currentIndex]);
+
+                    for (var i = (index + 4); (0 != highBits); ++i) {
+                        if (0 != (highBits & 0xF)) {
+                            valueListBuilder.Append(i);
+                        }
+
+                        highBits >>= 8;
+                    }
+                }
+
+                for (; (index < length); index++) {
+                    var c = Unsafe.Add(ref input, ((IntPtr)(uint)index));
+
+                    if ((c == delimiter) || (c == escapeSentinel)) {
+                        valueListBuilder.Append(index);
+                    }
+                }
+
+                return index;
+            }
+
+            var beginIndex = 0;
+            var isEscaping = false;
+            var length = input.Length;
+            var span = input.Span;
+            var valueListBuilder = new ValueListBuilder<int>(stackalloc int[32]);
+            var delimiterIndices = MakeDelimiterList(ref valueListBuilder, ref MemoryMarshal.GetReference(span), length, delimiter, escapeSentinel);
+            var loopLimit = delimiterIndices.Length;
+            var result = new ReadOnlyMemory<char>[(loopLimit + 1)];
+            var resultIndex = 0;
+            var stringBuilder = ReadOnlyMemory<char>.Empty;
+
+            for (var loopIndex = 0; ((loopIndex < loopLimit) && (beginIndex < length)); ++loopIndex) {
+                var endIndex = delimiterIndices[loopIndex];
+
+                if (escapeSentinel == span[endIndex]) {
+                    if (isEscaping) {
+                        if (stringBuilder.IsEmpty) {
+                            stringBuilder = input[beginIndex..endIndex];
                         }
                         else {
-                            if (previousIndex != currentIndex) {
-                                stringBuffer.Write(temp.Span);
-
-                                temp = value[previousIndex..currentIndex];
-                            }
-
-                            if (0 == stringBuffer.WrittenCount) {
-                                results.Add(temp);
-                            }
-                            else {
-                                stringBuffer.Write(temp.Span);
-                                results.Add(stringBuffer.WrittenSpan.ToArray());
-                                stringBuffer.Clear();
-                            }
-
-                            temp = ReadOnlyMemory<char>.Empty;
+                            stringBuilder = stringBuilder.Concat(input[beginIndex..endIndex]);
                         }
 
-                        previousIndex = ++currentIndex;
-                    }
-                } while (++loopIndex < loopLimit);
+                        beginIndex = (endIndex + 1);
+                        isEscaping = false;
 
-                if (0 == temp.Length) {
-                    results.Add(value[previousIndex..]);
-                }
-                else {
-                    if (previousIndex != value.Length) {
-                        stringBuffer.Write(temp.Span);
-
-                        temp = value[previousIndex..];
-                    }
-
-                    if (0 == stringBuffer.WrittenCount) {
-                        results.Add(temp);
+                        continue;
                     }
                     else {
-                        stringBuffer.Write(temp.Span);
-                        results.Add(stringBuffer.WrittenSpan.ToArray());
-                        stringBuffer.Clear();
+                        beginIndex = (endIndex + 1);
+                        isEscaping = true;
                     }
                 }
-            }
-            else {
-                results.Add(value);
+
+                if (!isEscaping) {
+                    var segment = input[beginIndex..endIndex];
+
+                    if (stringBuilder.IsEmpty && !segment.IsEmpty) {
+                        result[resultIndex] = segment;
+                    }
+                    else if (!stringBuilder.IsEmpty) {
+                        if (segment.IsEmpty) {
+                            result[resultIndex] = stringBuilder;
+                        }
+                        else {
+                            result[resultIndex] = stringBuilder.Concat(segment);
+                        }
+
+                        stringBuilder = ReadOnlyMemory<char>.Empty;
+                    }
+
+                    beginIndex = (endIndex + 1);
+                    ++resultIndex;
+                }
             }
 
-            return results.AsReadOnly();
+            var finalSegment = ((beginIndex < length) && (0 <= loopLimit)) ? input[beginIndex..] : ReadOnlyMemory<char>.Empty;
+
+            if (stringBuilder.IsEmpty && !finalSegment.IsEmpty) {
+                result[resultIndex] = finalSegment;
+            }
+            else if (!stringBuilder.IsEmpty) {
+                if (finalSegment.IsEmpty) {
+                    result[resultIndex] = stringBuilder;
+                }
+                else {
+                    result[resultIndex] = stringBuilder.Concat(finalSegment);
+                }
+            }
+
+            valueListBuilder.Dispose();
+
+            return result.AsMemory()[..(resultIndex + 1)];
         }
     }
 }
---
+++ Project/ValueListBuilder.cs
@@ -0,0 +1,74 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace ByteTerrace.Ouroboros.Core
+{
+    internal ref struct ValueListBuilder<T>
+    {
+        private T[]? m_array;
+        private int m_position;
+        private Span<T> m_span;
+
+        public ValueListBuilder(Span<T> initialSpan) {
+            m_span = initialSpan;
+            m_array = null;
+            m_position = 0;
+        }
+
+        public ref T this[int index] {
+            get {
+                Debug.Assert(index < m_position);
+                return ref m_span[index];
+            }
+        }
+        public int Length {
+            get => m_position;
+            set {
+                Debug.Assert(value >= 0);
+                Debug.Assert(value <= m_span.Length);
+                m_position = value;
+            }
+        }
+
+        private void Grow() {
+            var array = ArrayPool<T>.Shared.Rent(m_span.Length * 2);
+            var success = m_span.TryCopyTo(array);
+
+            Debug.Assert(success);
+
+            var toReturn = m_array;
+
+            m_span = m_array = array;
+
+            if (toReturn is not null) {
+                ArrayPool<T>.Shared.Return(toReturn);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Append(T item) {
+            var position = m_position;
+
+            if (position >= m_span.Length) {
+                Grow();
+            }
+
+            m_position = position + 1;
+            m_span[position] = item;
+        }
+        public ReadOnlySpan<T> AsSpan() {
+            return m_span[..m_position];
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose() {
+            var toReturn = m_array;
+
+            if (toReturn is not null) {
+                m_array = null;
+
+                ArrayPool<T>.Shared.Return(toReturn);
+            }
+        }
+    }
+}
