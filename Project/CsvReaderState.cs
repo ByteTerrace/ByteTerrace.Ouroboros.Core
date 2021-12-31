c9e228db82c864a5337f8fda51b501767def53f8
David Smith <kittoes0124@byteterrace.com>
2021-12-30 21:11:14 -0600

    check in

--- Project/CsvReaderState.cs
+++ Project/CsvReaderState.cs
@@ -9,39 +9,24 @@ namespace ByteTerrace.Ouroboros.Core
 {
     public ref struct CsvReaderState
     {
-        private readonly Vector256<ushort> m_carriageReturnVector;
-        private readonly Vector256<ushort> m_delimiterVector;
-        private readonly Vector256<ushort> m_escapeSentinelVector;
-        private readonly Vector256<ushort> m_lineFeedVector;
+        private readonly char m_delimiter;
+        private readonly char m_escapeSentinel;
 
         private Span<char> m_buffer;
-        private int m_bufferOffset;
+        private int m_bufferIndex;
         private uint m_bufferMask;
+        private int m_bufferOffset;
         private int m_currentControlCharIndex;
         private int m_numberOfCharsRead;
 
-        public ref char this[int index] {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => ref m_buffer[index];
-        }
-        public int CurrentControlCharIndex {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => m_currentControlCharIndex;
-        }
-        public Span<char> Span {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => m_buffer;
-        }
-
         public CsvReaderState(Span<char> buffer, char delimiter, char escapeSentinel) {
             m_buffer = buffer;
+            m_bufferIndex = -16;
             m_bufferMask = 0U;
             m_bufferOffset = buffer.Length;
-            m_carriageReturnVector = Vector256.Create('\r');
             m_currentControlCharIndex = -1;
-            m_delimiterVector = Vector256.Create(delimiter);
-            m_escapeSentinelVector = Vector256.Create(escapeSentinel);
-            m_lineFeedVector = Vector256.Create('\n');
+            m_delimiter = delimiter;
+            m_escapeSentinel = escapeSentinel;
             m_numberOfCharsRead = 0;
         }
 
@@ -59,29 +44,49 @@ namespace ByteTerrace.Ouroboros.Core
             return (0 < m_numberOfCharsRead);
         }
 
+        public ReadOnlyMemory<ReadOnlyMemory<char>> ReadNextRecord(TextReader reader) {
+            while (TryFindNextControlCharacter(reader)) {
+                if (m_delimiter == m_buffer[m_currentControlCharIndex]) {
+                }
+                else if (m_escapeSentinel == m_buffer[m_currentControlCharIndex]) {
+                }
+                else if ('\n' == m_buffer[m_currentControlCharIndex]) {
+                }
+                else if ('\r' == m_buffer[m_currentControlCharIndex]) {
+                }
+            }
+
+            return ReadOnlyMemory<ReadOnlyMemory<char>>.Empty;
+        }
         [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-        public bool FindNextControlCharacter(TextReader reader) {
+        public bool TryFindNextControlCharacter(TextReader reader) {
             if (0 != (m_bufferMask = Bmi1.ResetLowestSetBit(m_bufferMask))) {
-                m_currentControlCharIndex = (m_bufferOffset + ((int)(Bmi1.TrailingZeroCount(m_bufferMask) >> 1)) - 16);
+                m_currentControlCharIndex = (m_bufferIndex + ((int)(Bmi1.TrailingZeroCount(m_bufferMask) >> 1)));
 
                 return true;
             }
 
             if (CanReadFromBuffer16() || (TryFillBuffer(reader) && CanReadFromBuffer16())) {
+                var carriageReturnVector = Vector256.Create('\r');
+                var delimiterVector = Vector256.Create(m_delimiter);
+                var escapeSentinelVector = Vector256.Create(m_escapeSentinel);
+                var lineFeedVector = Vector256.Create('\n');
+
                 do {
                     var searchVector = LoadVector256(ref MemoryMarshal.GetReference(m_buffer), m_bufferOffset);
-                    var bufferMask = Avx2.MoveMask(Avx2.CompareEqual(m_carriageReturnVector, searchVector).AsByte());
+                    var bufferMask = Avx2.MoveMask(Avx2.CompareEqual(carriageReturnVector, searchVector).AsByte());
 
-                    bufferMask |= Avx2.MoveMask(Avx2.CompareEqual(m_delimiterVector, searchVector).AsByte());
-                    bufferMask |= Avx2.MoveMask(Avx2.CompareEqual(m_escapeSentinelVector, searchVector).AsByte());
-                    bufferMask |= Avx2.MoveMask(Avx2.CompareEqual(m_lineFeedVector, searchVector).AsByte());
+                    bufferMask |= Avx2.MoveMask(Avx2.CompareEqual(delimiterVector, searchVector).AsByte());
+                    bufferMask |= Avx2.MoveMask(Avx2.CompareEqual(escapeSentinelVector, searchVector).AsByte());
+                    bufferMask |= Avx2.MoveMask(Avx2.CompareEqual(lineFeedVector, searchVector).AsByte());
                     bufferMask &= 0b01010101010101010101010101010101;
 
+                    m_bufferIndex += 16;
                     m_bufferOffset += 16;
 
                     if (0 != bufferMask) {
                         m_bufferMask = ((uint)bufferMask);
-                        m_currentControlCharIndex = (m_bufferOffset + ((int)(Bmi1.TrailingZeroCount(m_bufferMask) >> 1)) - 16);
+                        m_currentControlCharIndex = (m_bufferIndex + ((int)(Bmi1.TrailingZeroCount(m_bufferMask) >> 1)));
 
                         return true;
                     }
@@ -89,24 +94,17 @@ namespace ByteTerrace.Ouroboros.Core
             }
 
             if (CanReadFromBuffer()) {
-                var carriageReturn = ((char)m_carriageReturnVector.GetElement(0));
-                var delimiter = ((char)m_delimiterVector.GetElement(0));
-                var escapeSentinel = ((char)m_escapeSentinelVector.GetElement(0));
-                var lineFeed = ((char)m_lineFeedVector.GetElement(0));
-                var numberOfCharsRead = m_numberOfCharsRead;
-
                 ref var buffer = ref MemoryMarshal.GetReference(m_buffer);
-                ref var offset = ref m_bufferOffset;
 
                 do {
-                    var c = Unsafe.Add(ref buffer, offset);
+                    var c = Unsafe.Add(ref buffer, m_bufferOffset);
 
-                    if ((carriageReturn == c) || (delimiter == c) || (escapeSentinel == c) || (lineFeed == c)) {
-                        m_currentControlCharIndex = offset;
+                    if ((m_delimiter == c) || (m_escapeSentinel == c) || ('\n' == c) || ('\r' == c)) {
+                        m_currentControlCharIndex = m_bufferOffset;
 
-                        return (++offset < numberOfCharsRead);
+                        return (++m_bufferOffset < m_numberOfCharsRead);
                     }
-                } while (++offset < numberOfCharsRead);
+                } while (++m_bufferOffset < m_numberOfCharsRead);
             }
 
             m_currentControlCharIndex = -1;
