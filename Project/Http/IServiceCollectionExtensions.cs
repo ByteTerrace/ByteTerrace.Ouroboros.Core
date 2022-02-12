7eb4ddeed68cbfab250831b0923189c03296aca0
David Smith <kittoes0124@byteterrace.com>
2022-02-12 00:00:00 -0600

    added experimental HttpController

---
+++ Project/Http/IServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ByteTerrace.Ouroboros.Http
+{
+    /// <summary>
+    /// A collection of extension methods that directly or indirectly augment the <see cref="IServiceCollection"/> class.
+    /// </summary>
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IHttpClientFactory"/> and related services to the collection and configures a binding between <see cref="IHttpClient"/> and the specified name.
+        /// </summary>
+        /// <param name="services">The collection of services that the client will be appended to.</param>
+        /// <param name="configureClient">A delegate that configures the client after instantiation.</param>
+        /// <param name="name">The name of the client.</param>
+        public static IHttpClientBuilder AddGenericHttpClient(
+            this IServiceCollection services,
+            Action<HttpClient> configureClient,
+            string name
+        ) =>
+            services.AddHttpClient<IHttpClient, GenericHttpClient>(
+                configureClient: configureClient,
+                name: name
+            );
+    }
+}
---
+++ Project/Mvc/HttpController.cs
@@ -0,0 +1,78 @@
+﻿using ByteTerrace.Ouroboros.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ByteTerrace.Ouroboros.Mvc
+{
+    [ApiController]
+    [Authorize]
+    public abstract class HttpController : ControllerBase
+    {
+        public IHttpClient HttpClient { get; init; }
+        public ILogger Logger { get; init; }
+
+        protected HttpController(
+            IHttpClientFactory httpClientFactory,
+            string httpClientName,
+            ILogger<HttpController> logger,
+            ITypedHttpClientFactory<GenericHttpClient> typedHttpClientFactory
+        ) {
+            var untypedHttpClient = httpClientFactory.CreateClient(name: httpClientName);
+
+            HttpClient = typedHttpClientFactory.CreateClient(httpClient: untypedHttpClient);
+            Logger = logger;
+        }
+
+        private async ValueTask<string> OnResponseReceived(HttpResponseMessage response) {
+            try {
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException e) {
+                if (Logger.IsEnabled(LogLevel.Trace)) {
+                    var statusCode = e.StatusCode;
+
+                    Logger.LogTrace(
+                        args: statusCode,
+                        exception: e,
+                        message: "Unhandled error code: {statusCode}."
+                    );
+                }
+
+                return e.Message;
+            }
+        }
+
+        [HttpGet("get")]
+        public virtual async ValueTask<ActionResult<string>> GetAsync(
+            string uri,
+            CancellationToken cancellationToken = default
+        ) => Ok(
+            value: await HttpClient.GetAsync(
+                cancellationToken: cancellationToken,
+                operation: HttpGetOperation<string>.New(
+                    callback: OnResponseReceived,
+                    uri: uri
+                )
+            )
+        );
+        [HttpGet("post")]
+        public virtual async ValueTask<ActionResult<string>> PostAsync(
+            Stream stream,
+            string uri,
+            CancellationToken cancellationToken = default
+        ) => Ok(
+            value: await HttpClient.PostAsync(
+                cancellationToken: cancellationToken,
+                operation: HttpPostOperation<string>.New(
+                    callback: OnResponseReceived,
+                    contentStream: stream,
+                    uri: uri
+                )
+            )
+        );
+    }
+}
